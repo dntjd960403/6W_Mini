dfe8e56eb7bf8dae171193a993b16d0b7131e22a
dntjd960403 <dntjd960403@gmail.com>
2022-10-22 12:00:29 +0900

    feat: cors 추가

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Http = require('http');
 const routes = require('./routes');
+var cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,7 @@ const port = process.env.EXPRESS_PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/', routes);
 

af5f7b28b496404cf8a5878fb780be872a1e0c8e
ihyun <okyee8335@naver.com>
2022-10-25 20:19:55 +0900

    Fix) signUp & logIn

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -8,68 +8,66 @@ const re_email =
     /^([\w\.\_\-])*[a-zA-Z0-9]+([\w\.\_\-])*([a-zA-Z0-9])+([\w\.\_\-])+@([a-zA-Z0-9]+\.)+[a-zA-Z0-9]{2,8}$/;
 
 const schema = Joi.object({
-  id: Joi.string().pattern(re_id).required(),
-  nickname: Joi.string().pattern(re_nickname).required(),
-  password: Joi.string().pattern(re_password).required(),
-  confirm: Joi.string().required(),
-  email: Joi.string().pattern(re_email).required(),
-  address: Joi.string().required(),
+    id: Joi.string().pattern(re_id).required(),
+    nickname: Joi.string().pattern(re_nickname).required(),
+    password: Joi.string().pattern(re_password).required(),
+    confirm: Joi.string().required(),
+    email: Joi.string().pattern(re_email).required(),
+    address: Joi.string().required(),
 });
 
 class UserController {
-  userService = new UserService();
-  //회원가입
-  signup = async (req, res, next) => {
-    try {
-      const { id, nickname, password, confirm, email, address } = req.body;
-      await schema.validateAsync(req.body);
-      const registerUserResult = await this.userService.signup( id, nickname, password, confirm, email, address );
-      res.status(200).json({message: registerUserResult})
-    } catch (error) {
-      res.status(412).json({ message: error.message })
-    }
-  };
+    userService = new UserService();
+    //회원가입
+    signup = async (req, res, next) => {
+        try {
+            const {id, nickname, password, confirm, email, address} = req.body;
+            await schema.validateAsync(req.body);
+            const registerUserResult = await this.userService.signup(id, nickname, password, confirm, email, address);
+            res.status(200).json({message: registerUserResult})
+        } catch (error) {
+            res.status(412).json({errorMessage: error.message})
+        }
+    };
 
-  //로그인
-  login = async (req, res, next) => {
-    try {
-      const { id, password } = req.body;
-      const loginResult = await this.userService.login(id, password);
-      // console.log(user)
-      res.status(200).json(loginResult);
-    } catch (error) {
-      console.log(`${error.message}`);
-      res.status(400).send({errorMessage: "요청한 데이터 형식이 올바르지 않습니다."});
-    }
-  };
+    //로그인
+    login = async (req, res, next) => {
+        try {
+            const {id, password} = req.body;
+            const loginResult = await this.userService.login(id, password);
+            // console.log(user)
+            res.status(200).json(loginResult);
+        } catch (error) {
+            console.log(`${error.message}`);
+            res.status(400).send({errorMessage: error.message});
+        }
+    };
 
-  //비밀번호 분실시 아이디, 이메일로 회원 확인하여 비밀번호 변경
-  changePassword = async (req, res, next) => {
-    try {
-      const { id, password, confirm, email}  = req.body;
-      const { userId } = res.locals.user;
-      const changePasswordResult = await this.userService.changePassword(id, password, confirm, email, userId);
-      res.status(200).json({message: changePasswordResult});
-    } catch (error) {
-      console.log(`${error.message}`);
-      res.status(400).send({errorMessage: "요청한 데이터 형식이 올바르지 않습니다."});
-    }
-  };
+    //비밀번호 분실시 아이디, 이메일로 회원 확인하여 비밀번호 변경
+    changePassword = async (req, res, next) => {
+        try {
+            const {id, password, confirm, email} = req.body;
+            const {userId} = res.locals.user;
+            const changePasswordResult = await this.userService.changePassword(id, password, confirm, email, userId);
+            res.status(200).json({message: changePasswordResult});
+        } catch (error) {
+            console.log(`${error.message}`);
+            res.status(400).send({errorMessage: error.message});
+        }
+    };
 
-  //관리자 권한 임명
-  getAdmin = async (req, res, next) => {
-    try {
-      const { id } = req.body;
-      const user = await this.userService.getAdmin(id);
+    //관리자 권한 임명
+    getAdmin = async (req, res, next) => {
+        try {
+            const {id} = req.body;
+            const user = await this.userService.getAdmin(id);
 
-      res.status(200).json({ message: '관리자 임명 완료', id: user });
-    } catch (error) {
-      console.log(`${error.message}`);
-      res
-        .status(400)
-        .send({ errorMessage: '요청한 데이터 형식이 올바르지 않습니다.' });
-    }
-  };
+            res.status(200).json({message: '관리자 임명 완료', id: user});
+        } catch (error) {
+            console.log(`${error.message}`);
+            res.status(400).send({errorMessage: error.message});
+        }
+    };
 }
 
 module.exports = UserController;

a4aa14c02719ac77e5550b59bae64aec46bf6c09
dntjd960403 <dntjd960403@gmail.com>
2022-10-27 00:56:03 +0900

    feat swagger

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Http = require('http');
 const routes = require('./routes');
 const cors = require('cors');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
 
 const app = express();
 const http = Http.createServer(app);
@@ -11,6 +13,7 @@ const port = process.env.EXPRESS_PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(
   cors({
     origin: '*',
--- repositories/user.repositories.js
+++ repositories/user.repositories.js
@@ -1,48 +1,48 @@
 const { Users } = require('../models');
 
 class UserRepository {
-    //아이디로 회원 조회
-    findUserById = async (id) => {
-        const user = await Users.findOne({where: {id}});
-        return user;
-    }
+  //아이디로 회원 조회
+  findUserById = async (id) => {
+    const user = await Users.findOne({ where: { id } });
+    return user;
+  };
 
-    //유저아이디(PK)로 회원 조회
-    findUserByUserId = async (userId) => {
-        const user = await Users.findOne({where: {userId}});
-        console.log(user);
-        return user;
-    };
+  //유저아이디(PK)로 회원 조회
+  findUserByUserId = async (userId) => {
+    const user = await Users.findOne({ where: { userId } });
+    console.log(user);
+    return user;
+  };
 
-    //닉네임으로 회원 조회
-    findUserByNickname = async (nickname) => {
-        const user = await Users.findOne({where: {nickname}});
-        return user;
-    };
+  //닉네임으로 회원 조회
+  findUserByNickname = async (nickname) => {
+    const user = await Users.findOne({ where: { nickname } });
+    return user;
+  };
 
-    //회원가입
-    signup = async (id, nickname, password, email, address) => {
-        const registerUser = await Users.create({
-            id,
-            nickname,
-            password,
-            email,
-            address,
-        });
-        return registerUser;
-    };
+  //회원가입
+  signup = async (id, nickname, password, email, address) => {
+    const registerUser = await Users.create({
+      id,
+      nickname,
+      password,
+      email,
+      address,
+    });
+    return registerUser;
+  };
 
-    //비밀번호 변경
-    changePassword = async (id, email, password) => {
-        const changedPwData = await Users.update({password}, {where: {id, email}});
-        return changedPwData;
-    };
+  //비밀번호 변경
+  changePassword = async (id, email, password) => {
+    const changedPwData = await Users.update({ password }, { where: { id, email } });
+    return changedPwData;
+  };
 
-    //관리자 권한 임명
-    getAdmin = async (id) => {
-        const getAdmin = await Users.update({admin: null}, {where: {id}});
-        return getAdmin;
-    };
+  //관리자 권한 임명
+  getAdmin = async (id) => {
+    const getAdmin = await Users.update({ admin: null }, { where: { id } });
+    return getAdmin;
+  };
 }
 
 module.exports = UserRepository;

5c3645e8c2d5762396db23038a46f4e146586e6a
Damiyd <dpekfa144@gmail.com>
2022-10-26 22:13:47 +0900

    fix: requests,reviews CRUD 수정

--- repositories/requests.repository.js
+++ repositories/requests.repository.js
@@ -1,6 +1,7 @@
 const { requests } = require('../models');
 
 class RequestsRepository {
+  // 상품요청 전체 글보기
     findAllRequest = async () => {
     const request = await requests.findAll();
 
@@ -13,28 +14,28 @@ class RequestsRepository {
     return request;
 
   }
-  
+  // 상품요청 수정삭제 아이디값찾기
   RequestsById = async (requestId) => {
     const request = await requests.findByPk(requestId);
 
     return request;
 
   }
+// 상품요청 생성하기
+  createRequests = async (nickname,request) => {
 
-  createRequests = async (userId,request) => {
-
-    const createRequestsData = await requests.create({ userId,request });
+    const createRequestsData = await requests.create({ nickname,request });
 
     return createRequestsData;
   }
-
+// 상품요청 수정하기
   updateRequests = async (requestId,request) => {
     const updateRequestsData = await requests.update({request},{where: {requestId}});
 
     return updateRequestsData;
   }
  
-
+// 상품요청한거 삭제하기
   deleteRequests = async (requestId) => {
     const deleteRequestsData = await requests.destroy({ where: {requestId}});
 
